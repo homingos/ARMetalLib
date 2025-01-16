e67f25b69a6de3d6225818bfdd9ccda34f1b4502
Vishwas3000 <timeupvishwa@gmail.com>
2025-01-16 13:29:22 +0530

    first commit

---
+++ Sources/ARMetalLib/Core/LayerImage.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ARMetalLib
+//
+//  Created by Vishwas Prakash on 16/01/25.
+//
+
+import Foundation
---
+++ Sources/include/ARMetalLib.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  ARMetalLib
+//
+//  Created by Vishwas Prakash on 16/01/25.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
